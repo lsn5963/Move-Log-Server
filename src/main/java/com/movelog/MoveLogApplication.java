6a21a3df163deffdac2544286f6e484728780d2b
EunbeenDev <ros4312@naver.com>
2024-10-29 22:11:48 +0900

    initial commit

---
+++ src/main/java/com/movelog/MoveLogApplication.java
@@ -0,0 +1,13 @@
+package com.movelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoveLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/movelog/MoveLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
