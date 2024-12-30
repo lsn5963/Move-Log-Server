5005e143bfc62743b357e660bd1fba3873c46f4d
EunbeenDev <ros4312@naver.com>
2024-12-31 02:10:29 +0900

    [FEAT] database, oauth2 yml 설정 활성화

--- src/main/java/com/movelog/MoveLogApplication.java
+++ src/main/java/com/movelog/MoveLogApplication.java
@@ -8,8 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-//@PropertySource(value = { "classpath:oauth2/application-oauth2.yml" }, factory = YamlPropertySourceFactory.class)
-//@PropertySource(value = { "classpath:database/application-database.yml" }, factory = YamlPropertySourceFactory.class)
+@PropertySource(value = { "classpath:oauth2/application-oauth2.yml" }, factory = YamlPropertySourceFactory.class)
+@PropertySource(value = { "classpath:database/application-database.yml" }, factory = YamlPropertySourceFactory.class)
 //@PropertySource(value = { "classpath:s3/application-s3.yml" }, factory = YamlPropertySourceFactory.class)
 public class MoveLogApplication {
 
